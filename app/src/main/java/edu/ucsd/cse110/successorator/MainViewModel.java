b0680f808b825a1780cede4a9b89573c5225366c
Jacob <me@rooty.dev>
2024-02-09 01:23:29 -0800

    Hook up DateOffset into app logic

--- app/src/main/java/edu/ucsd/cse110/successorator/MainViewModel.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/MainViewModel.java
@@ -6,10 +6,14 @@ import static androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.APPLI
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.viewmodel.ViewModelInitializer;
 
+import edu.ucsd.cse110.successorator.lib.domain.DateOffset;
 import edu.ucsd.cse110.successorator.lib.domain.GoalRepository;
+import edu.ucsd.cse110.successorator.lib.util.MutableSubject;
+import edu.ucsd.cse110.successorator.lib.util.Subject;
 
 public class MainViewModel extends ViewModel {
     private final GoalRepository goalRepository;
+    private final MutableSubject<DateOffset> dateOffset;
 
     public static final ViewModelInitializer<MainViewModel> initializer =
             new ViewModelInitializer<>(
@@ -18,11 +22,26 @@ public class MainViewModel extends ViewModel {
                         var app =
                                 (SuccessoratorApplication) creationExtras.get(APPLICATION_KEY);
                         assert app != null;
-                        return new MainViewModel(app.getGoalRepository());
+                        return new MainViewModel(app.getGoalRepository(), app.getDateOffset());
                     }
             );
 
-    public MainViewModel(GoalRepository goalRepository) {
+    public MainViewModel(GoalRepository goalRepository, MutableSubject<DateOffset> offset) {
         this.goalRepository = goalRepository;
+        this.dateOffset = offset;
+    }
+
+    public Subject<DateOffset> getDateOffset() {
+        return dateOffset;
+    }
+
+    public void advance24Hours() {
+        DateOffset offset = dateOffset.getValue();
+        if (offset == null) {
+            return;
+        }
+
+        offset = offset.add(24 * 60 * 60);
+        dateOffset.setValue(offset);
     }
 }
--- app/src/main/java/edu/ucsd/cse110/successorator/SuccessoratorApplication.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/SuccessoratorApplication.java
@@ -6,10 +6,15 @@ import androidx.room.Room;
 
 import edu.ucsd.cse110.successorator.data.db.RoomGoalRepository;
 import edu.ucsd.cse110.successorator.data.db.SuccessoratorDatabase;
+import edu.ucsd.cse110.successorator.lib.domain.DateOffset;
 import edu.ucsd.cse110.successorator.lib.domain.GoalRepository;
+import edu.ucsd.cse110.successorator.lib.util.MutableSubject;
+import edu.ucsd.cse110.successorator.lib.util.SimpleSubject;
 
 public class SuccessoratorApplication extends Application {
     private GoalRepository goalRepository;
+    private MutableSubject<DateOffset> dateOffset;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -23,9 +28,17 @@ public class SuccessoratorApplication extends Application {
                 .build();
 
         this.goalRepository = new RoomGoalRepository(database.goalDao());
+
+        var dateOffset = new DateOffset(0);
+        this.dateOffset = new SimpleSubject<>();
+        this.dateOffset.setValue(dateOffset);
     }
 
     public GoalRepository getGoalRepository() {
         return goalRepository;
+    }
+
+    public MutableSubject<DateOffset> getDateOffset() {
+        return dateOffset;
     }
 }
