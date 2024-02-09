c38ead0331e734775e13dae6720ce5c9377a3148
Jacob <me@rooty.dev>
2024-02-09 00:31:10 -0800

    Setup tests

---
+++ lib/src/test/java/edu/ucsd/cse110/successorator/lib/domain/DateOffsetTest.java
@@ -0,0 +1,18 @@
+package edu.ucsd.cse110.successorator.lib.domain;
+
+import static org.junit.Assert.*;
+
+public class DateOffsetTest {
+
+    @org.junit.Test
+    public void getSeconds() {
+    }
+
+    @org.junit.Test
+    public void add() {
+    }
+
+    @org.junit.Test
+    public void now() {
+    }
+}
