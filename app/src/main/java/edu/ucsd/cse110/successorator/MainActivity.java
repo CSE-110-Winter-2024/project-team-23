c97fa36b2bf6efcf6a49db7f7293ab53d77ec85f
Jacob <me@rooty.dev>
2024-02-09 01:29:59 -0800

    UI elements to debug

--- app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
@@ -5,16 +5,31 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import edu.ucsd.cse110.successorator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private MainViewModel activityModel;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         var view = ActivityMainBinding.inflate(getLayoutInflater(), null, false);
         view.placeholderText.setText(R.string.hello_world);
+
+
+        var modelFactory = ViewModelProvider.Factory.from(MainViewModel.initializer);
+        var modelProvider = new ViewModelProvider(this, modelFactory);
+        this.activityModel = modelProvider.get(MainViewModel.class);
+
+        activityModel.getDateOffset().observe(offset -> {
+            if (offset == null) return;
+            view.dateDebugText.setText(offset.now().toString());
+        });
+
+        view.dateAdvanceButton.setOnClickListener(v -> activityModel.advance24Hours());
 
         setContentView(view.getRoot());
     }
