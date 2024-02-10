28846de715624447fe1bcbc491fca83d08aca380
Eric Hu <erichu100@outlook.com>
2024-02-09 20:40:24 -0800

    Plugged addGoal() into CreateDialogFragment. Properly adds to app's database.

--- app/src/main/java/edu/ucsd/cse110/successorator/ui/CreateGoalDialogFragment.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/ui/CreateGoalDialogFragment.java
@@ -2,23 +2,32 @@ package edu.ucsd.cse110.successorator.ui;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.Date;
+
+import edu.ucsd.cse110.successorator.MainViewModel;
 import edu.ucsd.cse110.successorator.R;
 import edu.ucsd.cse110.successorator.databinding.FragmentDialogCreateGoalBinding;
+import edu.ucsd.cse110.successorator.lib.domain.Goal;
 
 
 public class CreateGoalDialogFragment extends DialogFragment {
     private FragmentDialogCreateGoalBinding view;
+    //Not this most flexible name but the least ambiguous.
+    private MainViewModel mainViewModel;
+
     public CreateGoalDialogFragment() {
         // Required empty public constructor
     }
@@ -31,12 +40,29 @@ public class CreateGoalDialogFragment extends DialogFragment {
         return fragment;
     }
 
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        var modelOwner = requireActivity();
+        var modelFactory = ViewModelProvider.Factory.from(MainViewModel.initializer);
+        var modelProvider = new ViewModelProvider(modelOwner, modelFactory);
+        this.mainViewModel = modelProvider.get(MainViewModel.class);
+    }
+
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState){
         this.view = FragmentDialogCreateGoalBinding.inflate(getLayoutInflater());
         return new AlertDialog.Builder(getActivity())
                 .setView(view.getRoot())
+                //Figure out how to turn into image/checkmarks (ascii art? emojis? svg?)
+                .setPositiveButton("Checkmark", this::onCheckClick)
                 .create();
+    }
+
+    private void onCheckClick(DialogInterface dialog, int which){
+        String content = view.goalInput.getText().toString();
+        mainViewModel.addGoal(content);
+        dialog.dismiss();
     }
 }
