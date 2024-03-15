9c30bb4499cd493066e095089de88dc47d547ec0
wizard1238 <49770335+wizard1238@users.noreply.github.com>
2024-03-14 21:28:36 -0700

    reverted half of previous reversion from 'UI Bug Fixes + Goal Creation Functionality'

--- app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
@@ -5,7 +5,6 @@ import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
